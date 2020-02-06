bf835c85d649fa6f7305d226a3315101b5b2897b
jmjoy <918734043@qq.com>
2020-02-06 11:47:03 +0800

    Add benchmarks.

--- benches/main.rs
+++ benches/main.rs
@@ -1,1 +1,61 @@
+#![feature(test)]
 
+extern crate test;
+
+use test::Bencher;
+use async_wg::WaitGroup;
+use std::sync::atomic::AtomicUsize;
+use std::sync::atomic::Ordering;
+use std::sync::Arc;
+
+#[tokio::test]
+async fn wait_group_version() {
+    let count = Arc::new(AtomicUsize::new(0));
+    let wg = WaitGroup::new();
+
+    for _ in 0..10 {
+        let mut wg = wg.clone();
+        wg.add(1).await;
+        let count = count.clone();
+
+        tokio::spawn(async move {
+            count.fetch_add(1, Ordering::SeqCst);
+            wg.done().await;
+        });
+    }
+
+    wg.await;
+
+    assert_eq!(count.load(Ordering::SeqCst), 10);
+}
+
+#[tokio::test]
+async fn join_handle_version() {
+    let count = Arc::new(AtomicUsize::new(0));
+    let mut handles = Vec::with_capacity(10);
+
+    for _ in 0..10 {
+        let count = count.clone();
+
+        let handle = tokio::spawn(async move {
+            count.fetch_add(1, Ordering::SeqCst);
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.await.unwrap();
+    }
+
+    assert_eq!(count.load(Ordering::SeqCst), 10);
+}
+
+#[bench]
+fn bench_join_handle(b: &mut Bencher) {
+    b.iter(|| join_handle_version());
+}
+
+#[bench]
+fn bench_wait_group(b: &mut Bencher) {
+    b.iter(|| wait_group_version());
+}
